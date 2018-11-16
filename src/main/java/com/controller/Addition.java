551269c5edb0b38f9e0c79f9a85e0fb6e7443e7a
Manojk97 <manojnair097@gmail.com>
2018-11-16 12:51:14 +0530

    JenkinDemo

---
+++ src/main/java/com/controller/Addition.java
@@ -0,0 +1,48 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Addition
+ */
+public class Addition extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Addition() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		int n1=Integer.parseInt(request.getParameter("n1"));
+		int n2=Integer.parseInt(request.getParameter("n2"));
+		int sum=n1+n2;
+		
+		out.println("Addition is"+sum);
+		
+	}
+
+}
